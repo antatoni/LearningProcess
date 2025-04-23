45bd93db556962b7c3a9b547a699fad682102817
Antani Kalchev <antatoni@abv.bg>
2025-04-23 21:15:50 +0300

    making card components using react

---
+++ ReactLearning/ReactBroCode/broCodeProject2/src/components/Card.jsx
@@ -0,0 +1,12 @@
+const Card = () => {
+    return (
+        <div className='card'>
+            <img className="cardImage" alt="profile picture" src="https://scontent.fsof10-1.fna.fbcdn.net/v/t1.6435-1/62342886_2179935922091077_3647331123056345088_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=108&ccb=1-7&_nc_sid=e99d92&_nc_ohc=3Cd6GGL5FkEQ7kNvwH8xv45&_nc_oc=AdnIUNu4E_nbhW1XplC5NdRbxuXC6ozlBkJ3KY24GRgfIl1RO3XOD33tYvPOFKbVJQBSrDQD8u3kxTTFWi1_LfNx&_nc_zt=24&_nc_ht=scontent.fsof10-1.fna&_nc_gid=Aif5gIrxe0k0dktXwDjRiQ&oh=00_AfE9F3mIAlyKi1JuBM6j1ExpL2-SDhrDqQrh4kvmhaNowA&oe=683085D0"></img>
+            <h2 className="card-title">Antani Kalchev</h2>
+            <p className="card-desc">I'm a student , studying Javascript and anything frontend</p>
+        </div>
+    )
+}
+
+
+export default Card;
