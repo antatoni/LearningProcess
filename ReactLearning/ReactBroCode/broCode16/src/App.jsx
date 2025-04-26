3575859fd5d376c290bc4453c397c8c44edb5f82
Antani Kalchev <antatoni@abv.bg>
2025-04-26 23:03:48 +0300

    useRef hook

--- ReactLearning/ReactBroCode/broCode16/src/App.jsx
+++ ReactLearning/ReactBroCode/broCode16/src/App.jsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import MyComponent from './components/MyComponent'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  //useState() = re-renders the component when the state value changes !
+
+
+  //useRef() = Does not cause re-renders when its value changes ! 
+  //           1.Accessing/Interacting with DOM elements
+  //           2.Handling Focus , Animations and transitions
+  //           3.Managing timers and intervals.
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <MyComponent></MyComponent>
     </>
   )
 }
